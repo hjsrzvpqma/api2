deccc0ef552d7f391cc488120a66a014652628a0
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-04 21:00:32 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -90,7 +90,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'spherobot' then -- Put everything you like :)
+  if matches[1]:lower() == 'helper' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -119,7 +119,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "spherobot" then -- Put everything you like :)
+    if matches[2] == "helper" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,7 +141,7 @@ return {
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
     "^[!/]([Ss]tats) (spherobot)",-- Put everything you like :)
-		"^[!/](spherobot)"-- Put everything you like :)
+		"^[!/]()"-- Put everything you like :)
     }, 
   run = run
 }
