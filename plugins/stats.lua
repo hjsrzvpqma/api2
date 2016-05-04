0240c9c50d6b875baa44fff176b0ee816603458f
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-04 20:53:22 +0430

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,7 +91,7 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'sss' then -- Put everything you like :)
+  if matches[1]:lower() == 'helper' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
     savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
@@ -142,7 +142,7 @@ return {
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
     "^[!/]([Ss]tats) (helper)",-- Put everything you like :)
-		"^[!/](sss)"-- Put everything you like :)
+		"^[!/]()"-- Put everything you like :)
     }, 
   run = run
 }
