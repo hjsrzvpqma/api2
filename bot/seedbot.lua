fe9181fccfcc37e477b5d1dbb3efa053097be054
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-10 01:57:50 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,7 +227,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {85831686},--Sudo users
+    sudo_users = {90285047},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sphero-bot Helper v1.2beta
